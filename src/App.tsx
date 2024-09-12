7e3e024d539da3a9cca1f77529b4db62bf3283a7
Huzaifah Tariq <huzaifahtariq028@gmail.com>
2024-09-13 00:27:03 +0500

    integrated backend for user lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import Alert from './components/Alert';
-import Button from './components/Button';
-import Like from './components/Like';
-import ListGroup from './components/ListGroup';
-
 function App() {
-  return (
-    <div>
-      <Alert onClick={() => console.log('Alert Closed!')}>Test Alert!</Alert>
-      <ListGroup
-        items={['Lahore', 'Islamabad', 'Faisalabad']}
-        heading={'Cities'}
-        onSelectItem={(item) => console.log('Item clicked:', item)}
-      />
-      <Like onClick={() => console.log('Like Clicked!')} />
-      <Button onClick={() => console.log('Button Clicked!')}>
-        Test Button
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
